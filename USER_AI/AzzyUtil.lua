ef4ac50164c4f96da103b7d7eb3dc8397a9abd94
jknpj <jknpjr@gmail.com>
2025-07-12 00:43:53 -0300

    Oops

--- USER_AI/AzzyUtil.lua
+++ USER_AI/AzzyUtil.lua
@@ -2833,10 +2833,10 @@ function DiscoverOriginBySkill()
   if GetV(V_SKILLATTACKRANGE, MyID, HVAN_CAPRICE) == 9 then
     OldHomunType=VANILMIRTH
   -- 2. Check for Filir by checking the range of Moonlight
-  elseif GetV(V_SKILLATTACKRANGE, MyID, HFIL_MOONLIGHT) == 1 then
+  elseif GetV(V_SKILLATTACKRANGE, MyID, HFLI_MOON) == 1 then
     OldHomunType=FILIR
   -- 3. Check for Lif by checking the range of Healing Hands
-  elseif GetV(V_SKILLATTACKRANGE, MyID, HLIF_HEAL_HANDS) == 101 then
+  elseif GetV(V_SKILLATTACKRANGE, MyID, HLIF_HEAL) == 101 then
     OldHomunType=LIF
   -- 4. Check for Amistr by checking the range of Castling
   elseif GetV(V_SKILLATTACKRANGE, MyID, HAMI_CASTLE) == 102 then
